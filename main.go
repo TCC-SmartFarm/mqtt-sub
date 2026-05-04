d5909d0a9fc0949d2a80e0dadc5299e898fb566e
murilomolina <murilo.m.barone@gmail.com>
2026-05-04 16:15:42 -0300

    feat: implementação de fila dinamica apartir do nome do usuario (fazenda ou campo) do sensor

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 	"time"
 	"context"
+	"strings"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -17,47 +18,62 @@ import (
 type EventBus struct {
 	conn    *amqp.Connection
 	channel *amqp.Channel
-	queue   amqp.Queue
+	// queue   amqp.Queue removi pois a definição vai ser dinamica
 }
 
-// Inicializa a conexão com o barramento de eventos
+// Inicializa a conexão com o barramento de eventos // LEMBRAR DE MUDAR O NOME DO CONTAINER PARA CADA CASO DE DEPLOY!!!!
 func setupEventBus() *EventBus {
-	// Em produção (Docker), use: amqp://admin:admin@barramento-de-eventos:5672/
-	// conn, err := amqp.Dial("amqp://admin:admin@localhost:5672/")
-	conn, err := amqp.Dial("amqp://admin:admin@barramento-de-eventos:5672/")
+	// Em produção (Docker), use: "amqp://admin:admin@barramento-de-eventos:5672/"    
+	// "amqp://admin:admin@localhost:5672/"
+	var conn *amqp.Connection
+    var err error
+    url := "amqp://admin:admin@dev-barramento-de-eventos:5672/" 
 
-	if err != nil {
-		log.Fatalf("Falha ao conectar no RabbitMQ: %v", err)
-	}
+    // Tenta conectar 5 vezes antes de desistir
+    for i := 1; i <= 5; i++ {
+        fmt.Printf("Tentando conectar ao RabbitMQ (Tentativa %d/5)...\n", i)
+        conn, err = amqp.Dial(url)
+        if err == nil {
+            break
+        }
+        log.Printf("Aguardando RabbitMQ iniciar... %v", err)
+        time.Sleep(5 * time.Second) // Espera 5 segundos para a próxima tentativa
+    }
 
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatalf("Falha ao abrir canal no RabbitMQ: %v", err)
-	}
+    if err != nil {
+        log.Fatalf("Falha definitiva ao conectar no RabbitMQ: %v", err)
+    }
 
-	// Declara a fila onde os dados dos sensores serão acumulados
-	q, err := ch.QueueDeclare(
-		"fila_cache", // Nome da fila
-		true,                // Durável (sobrevive ao restart do RabbitMQ)
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		log.Fatalf("Falha ao declarar fila: %v", err)
-	}
+    ch, err := conn.Channel()
+    if err != nil {
+        log.Fatalf("Falha ao abrir canal no RabbitMQ: %v", err)
+    }
 
-	return &EventBus{conn: conn, channel: ch, queue: q}
+    fmt.Println("Conectado com sucesso ao RabbitMQ!")
+    return &EventBus{conn: conn, channel: ch}
 }
 
-func (eb *EventBus) publishToBus(payload []byte) {
+func (eb *EventBus) publishToBus(payload []byte, nomeDaFila string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err := eb.channel.PublishWithContext(ctx,
+	// Garante que a fila existe antes de enviar
+    _, err := eb.channel.QueueDeclare(
+        nomeDaFila, // Agora usamos o nome passado por parâmetro
+        true,       // Durável
+        false,
+        false,
+        false,
+        nil,
+    )
+    if err != nil {
+        log.Printf("Erro ao declarar fila: %v", err)
+        return
+    }
+
+	err = eb.channel.PublishWithContext(ctx,
 		"",            // exchange
-		eb.queue.Name, // routing key (nome da fila)
+		nomeDaFila, // routing key (nome da fila)
 		false,
 		false,
 		amqp.Publishing{
@@ -69,7 +85,7 @@ func (eb *EventBus) publishToBus(payload []byte) {
 	if err != nil {
 		log.Printf("Erro ao publicar no barramento: %v", err)
 	} else {
-		fmt.Printf("[EVENT BUS] Mensagem enviada para a fila: %s\n", eb.queue.Name)
+		fmt.Printf("[EVENT BUS] Mensagem enviada para a fila: %s\n", nomeDaFila)
 	}
 }
 
@@ -81,19 +97,28 @@ func main() {
 
 	// 2. Configura o MQTT
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker("tcp://mqtt-broker:1883")
+	opts.AddBroker("tcp://dev-mqtt-broker:1883")
 	opts.SetClientID("mqtt-sub")
-	opts.SetUsername("sensor")
-	opts.SetPassword("123")
+	opts.SetUsername("mqtt_sub")
+	opts.SetPassword("mqtt_sub")
 	opts.SetCleanSession(false)
 
 	opts.OnConnect = func(c mqtt.Client) {
 		fmt.Println("Ingestor conectado ao Broker MQTT")
 		c.Subscribe("campo/+/sensor/+/dados", 1, func(client mqtt.Client, msg mqtt.Message) {
-			fmt.Printf("\nMQTT Recebido: %s", string(msg.Payload()))
-			
-			// ENVIAR PARA O BARRAMENTO
-			bus.publishToBus(msg.Payload())
+			fmt.Printf("\nMQTT Recebido: %s\nTópico: %s", string(msg.Payload()), msg.Topic())
+			topic := msg.Topic()
+        	parts := strings.Split(topic, "/")
+        
+			if len(parts) >= 4 {
+				campoId := parts[1] // campoId presente no tópico (nome da fazenda/ id da fazenda/ do usuario)
+				nomeFila := fmt.Sprintf("fila_%s", campoId)
+				
+				fmt.Printf("\n[MQTT] Processando Sensor: %s", campoId)
+				
+				// Publica na fila específica do dispositivo
+				bus.publishToBus(msg.Payload(), nomeFila)
+			}
 		})
 	}
 
